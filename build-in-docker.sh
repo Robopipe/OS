a810c78d8d1f9927c7ca24869b5c3ceb50d98b4c
Adam Berkeš <adam.berkes@koala42.com>
2024-12-16 13:41:05 +0100

    Specify platform in build in docker

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --privileged --rm -v $(pwd):/mnt debian:bookworm bash -c "INSTALL_DEPS=true /mnt/build-os.sh /mnt/data /mnt/archive.swu"
+docker run --platform linux/arm64 \
+    --privileged --rm \
+    -v $(pwd):/mnt debian:bookworm bash \
+    -c "INSTALL_DEPS=true /mnt/build-os.sh /mnt/data /mnt/archive.swu"
